48c44d91041f6114b884295481d08f0e4626b8cf
allen1208227 <allen1208227@gmail.com>
2019-03-20 00:16:22 +0800

    done

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
-void reset_handler(void)
+void reset_handler_1(void)
 {
-	while (1)
-		;
+	int a=1;
+	a=reset_handler_2(a);
 }
+
+
+int reset_handler_2(int a)
+{
+	a=a+1;
+	a=reset_handler_3(a);
+	return a;	
+}
+
+int reset_handler_3(int a)
+{
+	int b=6;
+	b=a*b;
+	return b;
+}
+ 
+
--- vector_table.s
+++ vector_table.s
@@ -1,4 +1,11 @@
 .section .vector_table
 
 .word 0x20000100
-.word reset_handler
+.word _start
+
+.global _start
+.type _start, %function
+_start: b reset_handler_1
+sleep:
+	b	sleep
+
